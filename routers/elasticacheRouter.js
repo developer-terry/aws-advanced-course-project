c613715722d7edcc7d6c255e12034e744bf47bab
EC2 Default User <ec2-user@ip-172-31-23-25.ap-northeast-1.compute.internal>
2023-04-08 17:42:51 +0000

    add cache

--- routers/elasticacheRouter.js
+++ routers/elasticacheRouter.js
@@ -18,7 +18,19 @@ router.get("/elasticache/list_employee", async function (req, res) {
 router.get("/elasticache/list_employee_cached", async function (req, res) {
   console.log("/elasticache/list_employee_cached called");
   const start_time = new Date().getTime();
+  
+  // get cache
+  const cache_result = await awsElasticacheService.get(emp_list_key);
+  if(cache_result) {
+    console.log('emp_list_key exist');
+    res.send({"result": cache_result, "processed_time": myUtilService.get_process_time(start_time)});
+    return;
+  }
+  
+  // main logic
   const result = await empModelService.list_employee();
+  // set cache
+  await awsElasticacheService.set(emp_list_key, result);
   res.send({"result": result,"processed_time": myUtilService.get_process_time(start_time)});
 });
 
